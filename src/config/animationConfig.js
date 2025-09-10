c484a74501f3df6a34ccac1f483b8ddf0003bf88
Warren <warren.r.davison@gmail.com>
2025-09-10 12:46:50 -0400

    adjust choreography

--- src/config/animationConfig.js
+++ src/config/animationConfig.js
@@ -1,9 +1,9 @@
 // config object to hold all variables
 export const animationConfig = {
   storyId: '280b886368de43c18e18490f18a3ba5a',
+  mapId: 'a8ca37e1485b47af8a6bf6502330eb2c',
   nodeSelector: '#n-6IVlzF',
   choreographyPath: '../data/mapChoreography.json',
   debugMode: true,
-  disableMapNav: true,
-  mapId: 'a8ca37e1485b47af8a6bf6502330eb2c'
+  disableMapNav: true
 };
