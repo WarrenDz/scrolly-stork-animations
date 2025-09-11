2a37b4311ff8623e6541fadb815d9b5970a1357d
Warren <warren.r.davison@gmail.com>
2025-09-10 20:02:49 -0400

    make map params animationConfig arguments

--- src/config/animationConfig.js
+++ src/config/animationConfig.js
@@ -2,6 +2,8 @@
 export const animationConfig = {
   storyId: '280b886368de43c18e18490f18a3ba5a',
   mapId: 'a8ca37e1485b47af8a6bf6502330eb2c',
+  mapCenter: '-83.240763, 41.996520',
+  mapZoom: '4',
   nodeSelector: '#n-6IVlzF',
   choreographyPath: '../data/mapChoreography.json',
   debugMode: true,
