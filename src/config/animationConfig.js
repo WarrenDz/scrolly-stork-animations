d49f4114a468b21adc595419f541e2f89eca867f
Warren Davison <49045641+WarrenDz@users.noreply.github.com>
2025-09-23 15:42:34 -0400

    Update animationConfig.js
    change storyID

--- src/config/animationConfig.js
+++ src/config/animationConfig.js
@@ -1,6 +1,6 @@
 // config object to hold all variables
 export const animationConfig = {
-  storyId: '280b886368de43c18e18490f18a3ba5a',
+  storyId: '6080d58106244ac1998f96821ae7d242',
   mapId: 'a8ca37e1485b47af8a6bf6502330eb2c',
   timePlayRate: '25',
   nodeSelector: '#n-6IVlzF',
