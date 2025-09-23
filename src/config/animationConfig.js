037cdf0cc4465330ffb69e5ec18e7b79b7e8d228
Warren Davison <49045641+WarrenDz@users.noreply.github.com>
2025-09-23 15:46:46 -0400

    Update animationConfig.js
    update node id

--- src/config/animationConfig.js
+++ src/config/animationConfig.js
@@ -3,7 +3,7 @@ export const animationConfig = {
   storyId: '6080d58106244ac1998f96821ae7d242',
   mapId: 'a8ca37e1485b47af8a6bf6502330eb2c',
   timePlayRate: '25',
-  nodeSelector: '#n-6IVlzF',
+  nodeSelector: '#n-RlFZzE',
   choreographyPath: '../data/mapChoreography.json',
   debugMode: true,
   disableMapNav: true
